c56240d419780e7a9f799f395fd7f0719ae03e0d
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-03 15:27:14 +0100

    make navbar respansiv

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import style from "./navbar.module.css"
 import { Link } from 'react-router-dom'
 
@@ -6,11 +6,14 @@ import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={style.navbar} >
 
             <h1>E-Commerce </h1>
-            <ul className={style.link}>
+           
+            <ul className={isOpen ? style.link : style.showMenu}>
+
                 <li><Link to="/" tabIndex="0"> Home </Link></li>
                 <li><Link to="/mensclothes" tabIndex="0">Men</Link> </li>
                 <li><Link to="/jewelry" tabIndex="0">Women</Link></li>
@@ -22,7 +25,9 @@ function Navbar() {
                 <p>Profile</p>
                 <p>Cart</p>
             </div>
-
+ <button className={style.burgerMenu} onClick={() => setIsOpen(!isOpen)}>
+                ☰
+            </button>
         </div >
     )
 }

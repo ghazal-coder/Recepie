4d4b85d96ef538733deccb7c7d343c631e5a9dca
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-04 18:53:08 +0100

    Add counter

--- src/component/Article/Article.jsx
+++ src/component/Article/Article.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import style from "./Article.module.css"
 
 function Article(props) {
      const { image,title,price } = props.article;
-     
+     const [qty,setqty]=useState(0)
+ 
+     function IncreaseQty(){
+setqty((qty)=>(qty+1))
+     }
 
+      function DecreaseQty(){
+    
+        qty>0 && setqty((qty)=>(qty-1))
+     }
     return (
         <div className={style.product}>
             <img className={style.img} src={props.article.image} alt={title} />
             <div className={style.title}>
                 <p className={style.h}>{props.article.price} £</p>
+
+                <div className={style.Btn}>   
+                 <button onClick={IncreaseQty} className={style.productBtn}>+</button>
+                <span>{qty}</span>
+                <button onClick={DecreaseQty}  className={style.productBtn}>-</button>
+                </div>
+
                 <button>Buy Now</button>
             </div>
         </div>
--- src/component/explore/explore.jsx
+++ src/component/explore/explore.jsx
@@ -1,10 +1,24 @@
 
+import { useEffect, useState } from "react"
 import style from "./explore.module.css"
 
 function explore() {
+    const[counter,setcounter]=useState(0)
+
+
+    useEffect( ()=>{
+setInterval(() => {
+    setcounter ( (counter) => (counter+1))
+    
+}, 1000);
+
+
+    },[])
+
     return (
         <div className={style.explore}>
-
+          <h2>You are still log in for : </h2>
+          <h2>{counter} sc</h2>
         </div>
     )
 }

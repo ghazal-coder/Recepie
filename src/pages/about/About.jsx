cd19fdbf0716bb52f3e20439187114d97ac83632
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-05 13:36:38 +0100

    use motion

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,15 +4,41 @@ import Navbar from '../../component/navbar/navbar'
 import Footer from '../../component/footer/Footer'
 import Explore from '../../component/explore/explore'
 import Wrapper from '../../component/container/Wrapper'
+import { motion } from "framer-motion";
+
+
+
+const listVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { staggerChildren: 0.2 } }
+};
+
+const itemVariants = {
+  hidden: { x: -10, opacity: 0 },
+  visible: { x: 0, opacity: 1 }
+};
 
 function About() {
-    return (
-        <div className={style.about}>
-            <Navbar />
-            <Wrapper><Explore /></Wrapper>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className={style.about}>
+      <Navbar />
+      <Wrapper>
+       
+        <motion.ul 
+          initial="hidden" 
+          animate="visible" 
+          variants={listVariants}
+          style={{ listStyle: "none", padding: 0 }}
+        >
+          <motion.li variants={itemVariants}>
+            <Explore />
+          </motion.li>
+         
+        </motion.ul>
+      </Wrapper>
+      <Footer />
+    </div>
+  );
 }
 
-export default About
+export default About;

886e3445fb4f702b9f48b8bb2455d8d1f2759f83
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-07 12:49:01 +0100

    Motion

--- src/component/motion/Motions.jsx
+++ src/component/motion/Motions.jsx
@@ -1,12 +1,29 @@
 import style from "./Motion.module.css";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { Link} from "react-router-dom";
+import { motion} from "framer-motion";
 
+
+
+const buttonVariants = {
+  
+    hover: {
+        scale:1.1,
+      
+        boxShadow: "0px 0px 8px rgb(27, 26, 26)",
+        
+      transition: {
+            // mirror | reverse | loop
+            repeatType: "reverse ",
+            repeat:2
+        }
+    }
+}
 function Motions() {
     return (
         <>
             <div className={style.explore}>
-          
+      
+    
                 <motion.div
                     className={style.Contactus}
                     initial={{ y: '-20vw'}}
@@ -24,8 +41,29 @@ function Motions() {
 
                     <hr />
                 </motion.div>
+   <motion.h2
+    animate={{ fontSize: "30px", color: "#11111" }}
+    transition={{ delay: 1.5 }}
+    style={{ textShadow: "2px 2px 5px rgba(10, 10, 10, 0.5)" }}
+ 
+>
+    E-Commerce <motion.span 
+        style={{ display: "inline-block" }} 
+        animate={{ y: [0, 18, 0], rotate: [0, 28, 0] }} // انیمیشن برای "Shop"
+        transition={{ delay: 1.8, duration: 0.5, repeat: 2, repeatType: "reverse" }}
+    >
+        Shop
+    </motion.span>
+</motion.h2>
 
-         
+
+
+
+        
+
+
+
+ 
                 <motion.div
                     className={style.btns}
                     initial={{ opacity: 0 }}
@@ -79,6 +117,9 @@ function Motions() {
                       <Link to="/products">
                                 <motion.button
                                     className={style.btn}
+                                      initial= {{x:920}}
+                                    animate={{x:0}}
+                                    transition={{ delay:1 , duration : 2, type:'spring'}}
                                     whileHover={{
                                         scale: 1.2,
                                         backgroundColor: " rgba(113, 125, 113, 0.8)",
@@ -118,6 +159,19 @@ function Motions() {
                                My Shopping
                                 </motion.span>
                 </motion.div>
+                  <motion.div
+                   
+            initial={{ opacity:0 }}
+            animate={{ opacity:1 }}
+            transition={{ delay:1, duration:2 }}
+        >
+                    <motion.button   className={style.btnJuorney}
+                   
+                    variants={buttonVariants}
+                    whileHover = "hover"
+                >
+                   Start Your Juorney
+                </motion.button></motion.div>
             </div>
         </>
     );
@@ -131,19 +185,5 @@ export default Motions;
 
 
   
-//         <div className={style.explore} >
-//         <div className={style.Contactus}>    <p>Contact us</p><hr />    </div>
-        
-//           <div> <motion.ul className={style.navbar}  initial={{opacity : 0}}
-//            animate={{opacity :3}}
-//             transition={{ delay:1 , duration : 2}}>
-               
-//                 <li><Link to="/mensclothes" tabIndex="0">Men</Link> </li>
-//                 <li><Link to="/jewelry" tabIndex="0">Women</Link></li>
-//                 <li><Link to="/" tabIndex="0">Baby Colection</Link></li>
-              
-//             </motion.ul>
-//    </div></div>
-//          <Link to="/products"><button className={style.btn}>Shop Now</button></Link> 
-   
+
        
